06331172ad891fc85d666a10a10b9cbd71761b30
P0L3 <andrija.2@hotmail.com>
2024-06-17 13:43:16 +0200

    Adds model pretrainig of roberta from scratch - NOTE: Tokneizer reqires merges.txt and vocab.*

---
+++ PRETRAINING/model_training_fromscratch_roberta.py
@@ -0,0 +1,89 @@
+import torch
+from transformers import RobertaTokenizer, RobertaForMaskedLM, DataCollatorForLanguageModeling, TrainingArguments, Trainer, RobertaConfig
+from datasets import load_dataset
+from os import mkdir
+
+DATA = "ED4RE_MSL512_ASL50_S3592675"
+DIR_TRAIN = f"DATASET/BATCHED/{DATA}_train/*.arrow"
+DIR_TEST = f"DATASET/BATCHED/{DATA}_test/*.arrow"
+MODEL = "p0l3/clireroberta_clirevocab_cased"
+BATCH = 24
+
+CHKPT = "MODELS/{}_{}_{}".format(MODEL.replace("/", "__"), DATA, BATCH)
+LOGS = CHKPT + "/LOGS"
+
+print("Checking for CUDA: ", torch.cuda.is_available())
+try:
+    mkdir(CHKPT)
+    print("Checkpoint folder created: ", CHKPT)
+except:
+    print("Checkpoint folder already exists: ", CHKPT)
+
+try:
+    mkdir(LOGS)
+    print("Logs folder created: ", LOGS)
+except:
+    print("Logs folder already exists: ", LOGS)
+
+# Load the saved dataset
+print("Loading data ...")
+train = load_dataset("arrow", data_files=DIR_TRAIN)
+test = load_dataset("arrow", data_files=DIR_TEST)
+
+# Access the loaded dataset
+print(train)
+print(test)
+
+# Modle and tokenizer load
+print("Loading tokenizer and model ...")
+tokenizer = RobertaTokenizer(vocab_file="LOCAL_MODELS/CliReRoBERTa/vocab.json", merges_file="LOCAL_MODELS/CliReRoBERTa/merges.txt")
+config = RobertaConfig.from_json_file("LOCAL_MODELS/CliReRoBERTa/config.json")
+model = RobertaForMaskedLM(config)
+
+print("Mask token:", tokenizer.mask_token)
+print("CLS token:", tokenizer.cls_token)
+print("SEP token:", tokenizer.sep_token)
+print("PAD token:", tokenizer.pad_token)
+print("UNK token:", tokenizer.unk_token)
+
+# MLM data prep
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer, mlm=True, mlm_probability=0.2
+)
+
+# Training args
+training_args = TrainingArguments(
+    output_dir=CHKPT,          # output directory to where save model checkpoint
+    evaluation_strategy="steps",    # evaluate each `logging_steps` steps
+    overwrite_output_dir=True,      
+    num_train_epochs=10,            # number of training epochs, feel free to tweak
+    per_device_train_batch_size=BATCH, # the training batch size, put it as high as your GPU memory fits
+    gradient_accumulation_steps=8,  # accumulating the gradients before updating the weights
+    per_device_eval_batch_size=BATCH,  # evaluation batch size
+    logging_steps=1000,             # evaluate, log and save model checkpoints every 1000 step
+    save_steps=1000,                # Save steps
+    logging_dir=LOGS,               # Where logging steps go
+    # load_best_model_at_end=True,  # whether to load the best model (in terms of loss) at the end of training
+    # save_total_limit=3,           # whether you don't have much space so you let only 3 model weights saved in the disk
+)
+
+# Initialize traininer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=train['train'],
+    eval_dataset=test['train'],
+)
+
+# train the model
+trainer.train()
+
+# Saving model
+try:
+    mkdir(CHKPT+"/pretrained_final")
+    print("Final folder created: ", CHKPT+"/pretrained_final")
+except:
+    print("Final folder already exists: ", CHKPT+"/pretrained_final")
+
+trainer.model.save_pretrained(CHKPT+"/pretrained_final_10epochs", safe_serialization=False) # Use safe_serialization=False when loading model in older versions of transformers (eg. 3.4.0)
